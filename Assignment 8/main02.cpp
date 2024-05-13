a1886fc3d61299567e22f51f940e42125ee42600
NamanR30 <144185320+NamanR30@users.noreply.github.com>
2024-05-13 14:50:51 -0400

    Add files via upload

---
+++ Assignment 8/main02.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include "Function02.h"
+#include "NonlinearSolver02.h"
+
+using namespace std;
+using namespace fre;
+
+int main() {
+    double F = 100.0;
+    double T = 3.0;
+    double C = 1.2;
+    double P = 98.56;
+    double Acc = 0.0001;
+    double LEnd = 0.0;
+    double REnd = 1.0;
+    double Guess = 0.2;
+
+    CouponFunc MyF3(F, T, C);
+    NonlinearSolver solver_nonlinear(P, LEnd, REnd, Acc, Guess);
+
+    cout << "Yield by bisection method: " << fixed << setprecision(4)
+         << solver_nonlinear.SolveByBisect(&MyF3);
+    cout << "\nYield by Newton-Raphson method: " << fixed << setprecision(4)
+         << solver_nonlinear.SolveByNR(&MyF3) << endl;
+
+    return 0;
+}
+
+//Output
+
+/*
+
+Yield by bisection method: 0.0168
+Yield by Newton-Raphson method: 0.0168
+
+*/
